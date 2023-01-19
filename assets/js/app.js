1ce3d8e12fd735bb34688e2984ca3d82c9be95ff
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-01-19 11:48:43 +0000

    Add js folder and app.js file. Add date under the page title

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+function getCurrentDay() {
+  return String(moment().toDate()).split(" ").slice(0, 4).join(" ");
+}
+
+function placeCurrentDateUnderTitle() {
+  const $titleDate = $("#currentDay");
+  $titleDate.text(getCurrentDay());
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  placeCurrentDateUnderTitle();
+});
